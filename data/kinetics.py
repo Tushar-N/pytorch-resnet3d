cc2b033cb25017631f633d9e2d82914fd498fb3d
Tushar Nagarajan <tushar.nagarajan@gmail.com>
2019-07-31 10:22:25 -0500

    glob.glob isn't guaranteed to sort frames

--- data/kinetics.py
+++ data/kinetics.py
@@ -22,6 +22,7 @@ def parse_annotations(root):
         for yt_id, start, end, label in tqdm.tqdm(zip(yt_ids, start_times, end_times, clip_labels), total=len(yt_ids)):
             label = label.strip('"')
             frames = glob.glob('%s/%s/%s_%06d_%06d/*.jpg'%(frame_dir, label, yt_id, int(start), int(end)))
+            frames = sorted(frames)
             frames = [f.replace(root, '') for f in frames]
             if len(frames)==0:
                 #print (yt_id, start, end, '-- Not present')
